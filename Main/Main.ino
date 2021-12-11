d08abe7234d2a56ffb52e0eabad781c8eaf56766
Sam Alrahhal <alrahhalsam23@gmail.com>
2021-12-11 03:20:15 +0100

    second change in a while

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,9 +1,3 @@
-#include <Servo.h>
-#include <Ethernet.h>
-#include<WiFi.h>
-
-
-
 int pin = 5;
 void setup(){
     pinMode(pin, INPUT);
@@ -11,9 +5,16 @@ void setup(){
 
 void loop()
 {
+    int temVar = 0;
     //amongus loop
     digitalWrite(pin, HIGH);
     delay(500);
     digitalWrite(pin, LOW);
     delay(500);
+    leesAlles(&temVar);
+}
+
+void leesAlles(int *alles)
+{
+    *alles = digitalRead(pin);
 }
