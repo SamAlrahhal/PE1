c8d703146da19efaed664b32e25c2a872e6800c8
Sam Alrahhal <alrahhalsam23@gmail.com>
2022-01-08 19:39:30 +0100

    standaard temlate voor het begin van onze project

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,23 +1,10 @@
 
-int pin = 5;
+
 void setup(){
-    pinMode(pin, INPUT);
-    Serial.begin(9600);
+
 }
 
 void loop()
 {
-    int temVar = 0;
-    //amongus loop
-    digitalWrite(pin, HIGH);
-    delay(500);
-    digitalWrite(pin, LOW);
-    delay(500);
-    leesAlles(&temVar);
-    delay(500);
-}
-
-void leesAlles(int *alles)
-{
-    *alles = digitalRead(pin);
+ 
 }
