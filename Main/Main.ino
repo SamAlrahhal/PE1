cc19d50f499dbf0af14fa184eb8e56e63a109a37
Sam Alrahhal <alrahhalsam23@gmail.com>
2022-02-02 00:15:15 +0100

    made 2 functions for authorized and unauthorized

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,77 +1,92 @@
-//Viral Science
-//RFID
+// Viral Science
+// RFID
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
- 
+
 #define SS_PIN 10
 #define RST_PIN 9
-#define LED_G 4 //define green LED pin
-#define LED_R 5 //define red LED
-#define BUZZER 2 //buzzer pin
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-Servo myServo; //define servo name
- 
-void setup() 
+#define LED_GREEN 4               // define green LED pin
+#define LED_RED 5                 // define red LED
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+Servo myServo;                    // define servo name
+
+void setup()
 {
-  Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
-  myServo.attach(3); //servo pin
-  myServo.write(0); //servo start position
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_R, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
-  noTone(BUZZER);
+  Serial.begin(9600); // Initiate a serial communication
+  SPI.begin();        // Initiate  SPI bus
+  mfrc522.PCD_Init(); // Initiate MFRC522
+  myServo.attach(3);  // servo pin
+  myServo.write(0);   // servo start position
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  digitalWrite(LED_RED, HIGH); // set the red led on
   Serial.println("Put your card to the reader...");
   Serial.println();
-
 }
-void loop() 
+
+void loop()
 {
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if (!mfrc522.PICC_IsNewCardPresent())
   {
     return;
   }
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  if (!mfrc522.PICC_ReadCardSerial())
   {
     return;
   }
-  //Show UID on serial monitor
+  // Show UID on serial monitor
   Serial.print("UID tag :");
-  String content= "";
+  String content = "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
+
+  for (byte i = 0; i < mfrc522.uid.size; i++)
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
+
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "89 B4 18 B9") //change here the UID of the card/cards that you want to give access
+
+  if (content.substring(1) == "89 B4 18 B9") // change here the UID of the card/cards that you want to give access
   {
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(500);
-    digitalWrite(LED_G, HIGH);
-    tone(BUZZER, 500);
-    delay(300);
-    noTone(BUZZER);
-    myServo.write(180);
-    delay(5000);
-    myServo.write(0);
-    digitalWrite(LED_G, LOW);
-  }else   {
-    Serial.println("Access denied");
-    digitalWrite(LED_R, HIGH);
-    tone(BUZZER, 300);
-    delay(1000);
-    digitalWrite(LED_R, LOW);
-    noTone(BUZZER);
+    authorized();
   }
-} 
+  else
+  {
+    unauthorized();
+  }
+
+}
+
+// turns on the green light and moves the servo motor
+void authorized()
+{
+  int beginPos = 0, endPos = 180;
+
+  Serial.println("Authorized access");
+  Serial.println();
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+
+  myServo.write(endPos);
+  delay(3000);
+  myServo.write(beginPos);
+
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_RED, HIGH);
+}
+
+//turns on the red light
+void unauthorized()
+{
+  Serial.println("Access denied");
+  digitalWrite(LED_RED, HIGH);
+}
